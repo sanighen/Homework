55602d28b5ddd1064067c99b1ddbb5dc70da7d96
Николай <@HOME-PC>
2020-03-13 22:53:34 +0200

    maybe done, maybe not

--- ObjectsNearEarth/src/main/App.java
+++ ObjectsNearEarth/src/main/App.java
@@ -6,7 +6,7 @@ public class App {
 
 	public static void main(String[] args) throws Exception {
 
-        new Asteroid().getNeoAsteroids("2020-03-01","2020-03-02");
+        new Asteroid().getNeoAsteroids("2020-02-27","2020-03-02");
 
 	}
 
--- ObjectsNearEarth/src/objects/Asteroid.java
+++ ObjectsNearEarth/src/objects/Asteroid.java
@@ -13,21 +13,21 @@ public class Asteroid {
 	private final static String API_KEY = "Ok7lflgXxbtbRKSwQXi78CxAfbLW69HR16YJMluj";
 	private final static String NEO_ENDPOINT = "https://api.nasa.gov/neo/rest/v1/feed";
 	Calendar calendar = Calendar.getInstance();
-	SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+	private final static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
 	public void getNeoAsteroids(String startDate, String endDate) throws Exception {
-		
+
 		calendar.setTime(format.parse(startDate));
-		
-//		for (int i = 1; i <= 2; i++) {
-			while (!startDate.equals(endDate)) {
+
+
+		while (!startDate.equals(endDate)) {
 			startDate = format.format(calendar.getTime());
-			
+
 			calendar.add(Calendar.DATE, 1);
-			
-			URL nasaUrl = new URL(NEO_ENDPOINT + "?start_date=" + startDate + "&end_date=" + endDate + "&api_key=" + API_KEY);
+
+			URL nasaUrl = new URL(NEO_ENDPOINT + "?start_date=" + startDate + "&end_date=" + startDate + "&api_key=" + API_KEY);
 			BufferedReader in = new BufferedReader(new InputStreamReader(nasaUrl.openStream()));
-			
+
 			String stringData = "";
 			String inputLine;
 			while ((inputLine = in.readLine()) != null)
@@ -40,58 +40,35 @@ public class Asteroid {
 
 			int count = data.getInt("element_count");
 
-			 for (int j = 0; j < count; j++) {
-				System.out.println();
-//				System.err.println("Data on asteroid №" + (j + 1));
-				
+			for (int j = 0; j < count; j++) {
+
+				System.err.println("Data on asteroid №" + (j + 1));
+
 				float diameter = data.getJSONObject("near_earth_objects")
 									 .getJSONArray(startDate)
 									 .getJSONObject(j)
 									 .getJSONObject("estimated_diameter")
 									 .getJSONObject("kilometers")
 									 .getFloat("estimated_diameter_min");
-				System.out.printf("Estimated diameter min: %.2f km%n", diameter);
+				System.out.printf("Minimum asteroid diameter: %.2f km%n", diameter);
 
 				double distance = data.getJSONObject("near_earth_objects")
-									  .getJSONArray(startDate)
-									  .getJSONObject(j)
+									  .getJSONArray(startDate).getJSONObject(j)
 									  .getJSONArray("close_approach_data")
 									  .getJSONObject(0)
 									  .getJSONObject("miss_distance")
 									  .getDouble("kilometers");
 				System.out.printf("Distance from object to Earth %.1f mln km %n", distance / 1000000);
-				
+
 				boolean isHazardous = data.getJSONObject("near_earth_objects")
-										  .getJSONArray(startDate)
-										  .getJSONObject(j)
+										  .getJSONArray(startDate).getJSONObject(j)
 										  .getBoolean("is_potentially_hazardous_asteroid");
 				System.out.println("Is potentially hazardous asteroid: " + isHazardous);
 
-
 			}
+			System.out.println();
 		}
 
-		
-
 	}
-	
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-//	public void setDate(String startDate) throws ParseException {
-//		Calendar calendar = Calendar.getInstance();
-//		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-//		calendar.setTime(format.parse(startDate));
-//		calendar.add(Calendar.DATE, 3);
-//		startDate = format.format(calendar.getTime());
-//	}
-	
 }
