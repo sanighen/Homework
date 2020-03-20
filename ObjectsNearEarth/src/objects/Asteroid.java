c96d5a949d26aaa52ab244e1a97d3d600ed76e8f
Николай <@HOME-PC>
2020-03-20 15:35:03 +0200

    fix

--- ObjectsNearEarth/src/objects/Asteroid.java
+++ ObjectsNearEarth/src/objects/Asteroid.java
@@ -16,30 +16,30 @@ public class Asteroid {
 	private final static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
 	public void getNeoAsteroids(String startDate, String endDate) throws Exception {
-
+		
+		URL nasaUrl = new URL(NEO_ENDPOINT + "?start_date=" + startDate + "&end_date=" + endDate + "&api_key=" + API_KEY);
+		BufferedReader in = new BufferedReader(new InputStreamReader(nasaUrl.openStream()));
+		
+		String stringData = "";
+		String inputLine;
+		while ((inputLine = in.readLine()) != null)
+			stringData += inputLine;
+		in.close();
+		
+		JSONObject data = new JSONObject(stringData);
+		
+		int count = data.getInt("element_count");
+		System.err.println("Found " + count + " asteroids");
+		
 		calendar.setTime(format.parse(startDate));
-
-		while (!startDate.equals(endDate)) {
+		
+		do {
 			startDate = format.format(calendar.getTime());
 			calendar.add(Calendar.DATE, 1);
 
-			URL nasaUrl = new URL(NEO_ENDPOINT + "?start_date=" + startDate + "&end_date=" + startDate + "&api_key=" + API_KEY);
-			BufferedReader in = new BufferedReader(new InputStreamReader(nasaUrl.openStream()));
-
-			String stringData = "";
-			String inputLine;
-			while ((inputLine = in.readLine()) != null)
-				stringData += inputLine;
-			in.close();
-
-			JSONObject data = new JSONObject(stringData);
-
-			int count = data.getInt("element_count");
-
 			System.out.println("----------------" + startDate + "----------------");
-			System.err.println("Found " + count + " asteroids");
 
-			for (int j = 0; j < count; j++) {
+			for (int j = 0; j < data.getJSONObject("near_earth_objects").getJSONArray(startDate).length(); j++) {
 				System.err.println("Data on asteroid №" + (j + 1));
 
 				float diameter = data.getJSONObject("near_earth_objects")
@@ -50,7 +50,8 @@ public class Asteroid {
 									 .getFloat("estimated_diameter_min");
 
 				double distance = data.getJSONObject("near_earth_objects")
-									  .getJSONArray(startDate).getJSONObject(j)
+									  .getJSONArray(startDate)
+									  .getJSONObject(j)
 									  .getJSONArray("close_approach_data")
 									  .getJSONObject(0)
 									  .getJSONObject("miss_distance")
@@ -67,57 +68,8 @@ public class Asteroid {
 
 			}
 			System.out.println();
-		}
-
+		} while (!startDate.equals(endDate));
 	}
-
-	public void getNeoAsteroids(String startDate) throws Exception {
-
-		URL nasaUrl = new URL(NEO_ENDPOINT + "?start_date=" + startDate + "&end_date=" + startDate + "&api_key=" + API_KEY);
-		BufferedReader in = new BufferedReader(new InputStreamReader(nasaUrl.openStream()));
-
-		String stringData = "";
-		String inputLine;
-		while ((inputLine = in.readLine()) != null)
-			stringData += inputLine;
-		in.close();
-
-		JSONObject data = new JSONObject(stringData);
-
-		int count = data.getInt("element_count");
-
-		System.out.println("----------------" + startDate + "----------------");
-		System.err.println("Found " + count + " asteroids");
-
-		for (int j = 0; j < count; j++) {
-
-			System.err.println("Data on asteroid №" + (j + 1));
-
-			float diameter = data.getJSONObject("near_earth_objects")
-								 .getJSONArray(startDate).getJSONObject(j)
-								 .getJSONObject("estimated_diameter")
-								 .getJSONObject("kilometers")
-								 .getFloat("estimated_diameter_min");
-
-			double distance = data.getJSONObject("near_earth_objects")
-								  .getJSONArray(startDate)
-								  .getJSONObject(j)
-								  .getJSONArray("close_approach_data")
-								  .getJSONObject(0)
-								  .getJSONObject("miss_distance")
-								  .getDouble("kilometers");
-
-			boolean isHazardous = data.getJSONObject("near_earth_objects")
-									  .getJSONArray(startDate)
-									  .getJSONObject(j)
-									  .getBoolean("is_potentially_hazardous_asteroid");
-			
-			System.out.printf("Minimum asteroid diameter: %.2f km%n", diameter);
-			System.out.printf("Distance from object to Earth %.1f mln km %n", distance / 1000000);
-			System.out.println("Is potentially hazardous asteroid: " + isHazardous);
-
-		}
-		System.out.println();
-	}
-
+	
+	
 }
